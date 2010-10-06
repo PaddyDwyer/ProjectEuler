132a7be791f0461591f649063553be84fb69335f
Patrick O'Dwyer <kertap_raid@yahoo.co.uk>
2010-10-06 18:51:18 -0400

    Starting off the problem by generating pandigital numbers

--- ruby/p38/p38.rb
+++ ruby/p38/p38.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env ruby -wKU
+
+def getPandigital(number)
+	pandigitalString = ""
+	i = 1
+	while pandigitalString.length < 9
+		pandigitalString << (number * i).to_s
+		i += 1
+	end
+	pandigitalString
+end
+
+p getPandigital(192)
+p getPandigital(1)

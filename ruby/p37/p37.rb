9d72bc4b01d0d534a6fef4481a07a0f229a13e62
Patrick O'Dwyer <kertap_raid@yahoo.co.uk>
2010-10-05 22:25:11 -0400

    at a good stage where near everything works
    need to add an mod_pow method as I'm getting overflow warnings

--- ruby/p37/p37.rb
+++ ruby/p37/p37.rb
@@ -1,12 +1,48 @@
 #!/usr/bin/env ruby -wKU -I ../lib
-require	'util'
+require 'util'
 
-$primes = generatePrimes 8000000
-#$primes = generatePrimes 1000
+# use the miller rabin test to check primality
+def isPrime?(n) 
+	return false if n == 1
+	nminus = n - 1
+	s = 0
+	d = nminus
+	while d % 2 == 0
+		d /= 2
+		s += 1
+	end
+	#puts "s #{s} d #{d}"
+	alla = []
+	if n < 4
+		alla = [2]
+	elsif n < 1373653
+		alla = [2, 3] 
+	elsif n < 9080191
+		alla = [31, 73]
+	elsif n < 4759123141
+		alla = [2, 7, 61]
+	end
+	alla.each do |a|
+		if (a ** d) % n != 1 && (0..(s-1)).all? { |r| (a ** ((2 ** r) * d)) % n != nminus }
+			return false
+		end
+	end
+	return true
+end
+isPrime? 3
 
-puts "primes generated"
+$endarray = ["3", "7"]
+$middlearray = ["1", "3", "7", "9"]
 
-def truncatable?(number)
+def createNumbers(numOfDigits)
+	ary = $endarray
+	numOfDigits.times { |i| ary = ary.product($middlearray) }
+	ary = ary.product($endarray)
+	ary.map { |a| a.flatten! ;a.reduce("") { |str, s| str + s }.to_i }
+end
+
+
+def isTruncatable?(number)
 	str = number.to_s
 	istruncatable = true
 	i = str.length - 1
@@ -14,7 +50,7 @@ def truncatable?(number)
 		left = str[0...(-i)].to_i
 		right = str[i..-1].to_i
 		#puts "left #{left} right #{right}"
-		if !$primes.include?(left) || !$primes.include?(right)
+		if !isPrime?(left) || !isPrime?(right)
 			istruncatable = false
 			break
 		end
@@ -23,20 +59,14 @@ def truncatable?(number)
 	istruncatable
 end
 
-selected = $primes.select { |p| p.to_s =~ /^[37][1379]*[37]$/ }
-p selected
-puts selected.size
-
-
-truncatable = selected.select { |p| truncatable? p }
-
-#$primes.each { |p|
-	#str = p.to_s
-	#if str.match(/^[37][1379]*[37]$/)
-		#truncatable << p #if truncatable? p
-	#end
-#}
+i = 0
+truncatable = []
+while truncatable.length < 10
+#while i < 1
+	nums = createNumbers(i)
+	#p nums
+	nums.each { |n| truncatable << n if isPrime?(n) && isTruncatable?(n) }
+	i += 1
+end
 
 p truncatable
-
-puts "size #{truncatable.length}"

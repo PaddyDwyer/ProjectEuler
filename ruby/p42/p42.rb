307fc41026b3a8c88e35335d35ff6dc8b7311e33
Patrick O'Dwyer <kertap_raid@yahoo.co.uk>
2010-10-10 14:47:49 -0400

    done

--- ruby/p42/p42.rb
+++ ruby/p42/p42.rb
@@ -2,6 +2,22 @@
 
 wordstxt = File.new("words.txt")
 
-p wordstxt.read("\"")
+line = wordstxt.readline
 
+line.gsub!(/"/, "")
+words = line.split(",")
 
+def getValue(word)
+	word.bytes.reduce(0) { |sum, c| sum + c.to_i - 64 }
+end
+
+values = words.map { |w| getValue w }
+
+triangles = [1]
+n = 2
+while triangles[-1] < values.max
+	triangles << (n * (n + 1)) / 2
+	n += 1
+end
+
+p values.select { |v| triangles.include? v }.size
